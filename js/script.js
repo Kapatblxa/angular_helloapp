2dd3e22e42fed5fd6032889d95cfe3ce18ac13ae
evgeny balandin <evgeny balandin>
2017-08-06 15:14:11 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let arr = ['Cat', 'Dog', 'Dolphin', 'Monkey', 'Elephant', 'Dude'];
+for (i=0; i<arr.length; i++) {
+	console.log(arr[i]);
+}
+
+//let arr2 = ['run', 'box', "swim", 'ski', 'shoot'];
+//for (i=0; i<arr2.length; i++) {
+//	console
+//}
